3c34518db957bf732c55d4505399e06f16e8480a
Ants <code.herder@recombulator.net>
2020-07-15 20:03:16 -0400

    Use external randomizer for shuffling

--- Shuffle.cs
+++ Shuffle.cs
@@ -4,10 +4,8 @@ namespace Lobstermania
 {
     class ArrayShuffle
     {
-        public static void Shuffle<T>(T[] items)
+        public static void Shuffle<T>(T[] items, Random rand)
         {
-            Random rand = new Random();
-
             // For each spot in the array, pick
             // a random item to swap into that spot.
             for (int i = 0; i < items.Length - 1; i++)
